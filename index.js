1eea88fb80af8982cc16d5941e5c4ac91ee48ec0
victor039 <tor-vic@hotmail.com>
2023-05-04 15:57:14 -0500

    uploading eventos files

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button")
+
+btn.addEventListener("click", () => alert("Click en el boton"))
+
+$("button").click(function(){
+    console.log("Hola estoy utilizando jQuery");
+})
